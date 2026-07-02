d5f19c5d4a536012bbe03697c2c830c323230c2d
T Vikas <vikastvikas73@gmail.com>
2026-07-02 12:55:11 +0000

    Added Python programs

---
+++ .vscode/factorial.py
@@ -0,0 +1,7 @@
+n=int(input("enter the num"))
+result=1
+for i in range( 1, n+1):
+    result=result*i
+print(result)
+
+
---
+++ .vscode/palindrome.py
@@ -0,0 +1,5 @@
+name = input("enter the string:")
+if name  == name[::-1]:
+    print("palindrome")
+else:
+    print("not palindrome")
